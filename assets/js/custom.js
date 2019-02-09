659e372fafc7f2d06ddd0391fdf37f78f4c36275
Lenmuel Del Rosario <lenmuel.dlrs@gmail.com>
2019-02-09 14:21:20 +0000

    Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,50 +33,22 @@ $(document).ready(function(){
 	      e.preventDefault();
 	    
 	      var $form = $(this);
-		  var res = false;
 		  
-		  $('button[type="submit"]', $form).each(function()
-	        {
-	            $btn = $(this);
-	            $btn.prop('type','button' ); 
-	            $btn.prop('orig_label',$btn.text());
-	            $btn.text('Sending ...');
-	        });
-		  
-	          $.post($form.attr("action"), $form.serialize()).then(function() {
-	          	if(!res){
-	          		 $("form#reused_form").hide();
-	        		 $("#success_message").show();
-	        		 $("#error_message").hide();
-	        		 return res= false;
-	          	}
-	          	else
-	          	{
-		            $('#error_message').append('<ul></ul>');
-		
-		            jQuery.each(data.errors,function(key,val)
-		            {
-		                $('#error_message ul').append('<li>'+key+':'+val+'</li>');
-		            });
-		            $('#success_message').hide();
-		            $('#error_message').show();
-		
-		            //reverse the response on the button
-		            $('button[type="button"]', $form).each(function()
-		            {
-		                $btn = $(this);
-		                label = $btn.prop('orig_label');
-		                if(label)
-		                {
-		                    $btn.prop('type','submit' ); 
-		                    $btn.text(label);
-		                    $btn.prop('orig_label','');
-		                }
-		            });
-		            
-		        }
+	        $.post($form.attr("action"), $form.serialize()).then(function() {
+	          const Toast = Swal.mixin({
+				  toast: true,
+				  position: 'top-end',
+				  showConfirmButton: false,
+				  timer: 3000
+				});
+				
+				Toast.fire({
+				  type: 'success',
+				  title: 'Message sent Successfully'
+				})
 	        });
 	    });
+	    
 	  
 	  //scroll to top
 	  var scrolltotop={setting:{startline:100,scrollto:0,scrollduration:1e3,fadeduration:[500,100]},controlHTML:'<img src="https://i1155.photobucket.com/albums/p559/scrolltotop/arrow66.png" />',controlattrs:{offsetx:5,offsety:5},anchorkeyword:"#top",state:{isvisible:!1,shouldvisible:!1},scrollup:function(){this.cssfixedsupport||this.$control.css({opacity:0});var t=isNaN(this.setting.scrollto)?this.setting.scrollto:parseInt(this.setting.scrollto);t="string"==typeof t&&1==jQuery("#"+t).length?jQuery("#"+t).offset().top:0,this.$body.animate({scrollTop:t},this.setting.scrollduration)},keepfixed:function(){var t=jQuery(window),o=t.scrollLeft()+t.width()-this.$control.width()-this.controlattrs.offsetx,s=t.scrollTop()+t.height()-this.$control.height()-this.controlattrs.offsety;this.$control.css({left:o+"px",top:s+"px"})},togglecontrol:function(){var t=jQuery(window).scrollTop();this.cssfixedsupport||this.keepfixed(),this.state.shouldvisible=t>=this.setting.startline?!0:!1,this.state.shouldvisible&&!this.state.isvisible?(this.$control.stop().animate({opacity:1},this.setting.fadeduration[0]),this.state.isvisible=!0):0==this.state.shouldvisible&&this.state.isvisible&&(this.$control.stop().animate({opacity:0},this.setting.fadeduration[1]),this.state.isvisible=!1)},init:function(){jQuery(document).ready(function(t){var o=scrolltotop,s=document.all;o.cssfixedsupport=!s||s&&"CSS1Compat"==document.compatMode&&window.XMLHttpRequest,o.$body=t(window.opera?"CSS1Compat"==document.compatMode?"html":"body":"html,body"),o.$control=t('<div id="topcontrol">'+o.controlHTML+"</div>").css({position:o.cssfixedsupport?"fixed":"absolute",bottom:o.controlattrs.offsety,right:o.controlattrs.offsetx,opacity:0,cursor:"pointer"}).attr({title:"Scroll to Top"}).click(function(){return o.scrollup(),!1}).appendTo("body"),document.all&&!window.XMLHttpRequest&&""!=o.$control.text()&&o.$control.css({width:o.$control.width()}),o.togglecontrol(),t('a[href="'+o.anchorkeyword+'"]').click(function(){return o.scrollup(),!1}),t(window).bind("scroll resize",function(t){o.togglecontrol()})})}};scrolltotop.init();
