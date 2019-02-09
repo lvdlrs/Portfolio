fd8935c01a55c0c7fc9fcdba193bcd90f15fb607
Lenmuel Del Rosario <lenmuel.dlrs@gmail.com>
2019-02-09 05:05:21 +0000

    Updating Contact Form

--- assets/js/thanks.js
+++ assets/js/thanks.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 const Thanks = () => (
-  <div>
-    <h1>Thank you!</h1>
+ <div class="modal-header">
+    <h5 class="modal-title" id="exampleModalLabel">Send me a Message!</h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
   </div>
 );
 
